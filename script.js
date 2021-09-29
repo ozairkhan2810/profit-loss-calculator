f410478b4308fcc897b1b0716aa2bc2cdfdbb7e8
ozairkhan2810 <ozairkhan2810@gmail.com>
2021-09-29 14:54:56 +0530

    fix: negative value and no value

--- script.js
+++ script.js
@@ -11,23 +11,37 @@ function submitHandler() {
   var qty = Number(stocksQuantity.value);
   var curr = Number(currentPrice.value);
 
+  if(initialPrice.value.length === 0) {
+    outputBox.innerText = "Please enter Initial Price";
+  }
+  else if(stocksQuantity.value.length === 0)
+  outputBox.innerText = "Please enter Stock Quantity";
+  else if(currentPrice.value.length === 0)
+  outputBox.innerText = "Please enter current price";
+  else if(initialPrice.value < 0)
+  outputBox.innerText = "Please enter positive value";
+  else if(stocksQuantity.value < 0)
+  outputBox.innerText = "Please enter positive value";
+  else if(currentPrice.value < 0)
+  outputBox.innerText = "Please enter positive value";
+  else
   calculateProfitAndLoss(ip, qty, curr);
 }
 
 function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
     var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
+    var lossPercentage = ((loss / initial) * 100).toFixed(2);
 
     showOutput(
-      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+      `Hey, the loss is ${loss} and the loss percent is ${lossPercentage}%`
     );
   } else if (current > initial) {
     var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
+    var profitPercentage = ((profit / initial) * 100).toFixed(2);
 
     showOutput(
-      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+      `Hey, the profit is ${profit} and the profit percent is ${profitPercentage}%`
     );
   } else {
     showOutput(`No pain no gain and no gain no pain`);
@@ -35,5 +49,5 @@ function calculateProfitAndLoss(initial, quantity, current) {
 }
 
 function showOutput(message) {
-  outputBox.innerHTML = message;
+  outputBox.innerText = message;
 }
